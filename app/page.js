d54ecbb3c27352f7dafe3c4809e72582ae46fea3
Samir Nepal <78529930+samir7-7@users.noreply.github.com>
2024-12-30 19:34:45 +0545

    Update page.js
    UI changes

--- app/page.js
+++ app/page.js
@@ -1,48 +1,87 @@
-'use client'
+"use client";
 
 export default function Home() {
   return (
-    <main>
+    <main className="h-[85vh] flex flex-col justify-center">
       <script defer type="text/javascript" src="/js/main.js"></script>
       <script defer type="text/javascript" src="/js/mcCluskyMethod.js"></script>
-      <div id="container">
+      <div className id="container">
         <form name="form1">
-          <div className="container">
-            <div className="content css-content">
-              <div className='options'>
-                circle number
-                <select id='nEvents' onChange={() => updateN() && renderVenn()}>
-                  <option id='2' value={2}>2</option>
-                  <option id='3' value={3}>3</option>
-                </select><br />
-                <button type="button" onClick={() => simplifyAndDrawExpression()} value="calculate">Calculate</button>
-                <textarea defaultValue={`(A'u(BnC'))'`} id="inputExpression"></textarea>
+          <div className="w-[100vw] flex justify-center mb-2">
+            <div className="content css-content w-[80%]  flex justify-center">
+              <div
+                className="options flex flex-col
+              gap-3 w-[50%] items-center"
+              >
+                <div className="select flex gap-2 w-[150%] justify-center">
+                  <span className="font-semibold">circle number</span>
+                  <select
+                    className="w-15"
+                    id="nEvents"
+                    onChange={() => updateN() && renderVenn()}
+                  >
+                    <option id="2" value={2}>
+                      2
+                    </option>
+                    <option id="3" value={3}>
+                      3
+                    </option>
+                  </select>
+                </div>
+
+                <textarea
+                  placeholder="Enter your formula"
+                  defaultValue={`(A'u(BnC'))'`}
+                  id="inputExpression"
+                  className="border-[1px] border-black border-dotted w-[150%] p-3 md:w-[50%]"
+                ></textarea>
+                <button
+                  type="button"
+                  onClick={() => simplifyAndDrawExpression()}
+                  value="calculate"
+                  className="bg-blue-500 p-4 py-2 text-white rounded-lg"
+                >
+                  Calculate
+                </button>
               </div>
             </div>
           </div>
-          <div className='container'>
-            <div className='content'>
-              <canvas id="venn" width="500" height="500">
-              </canvas>
-              <div dangerouslySetInnerHTML={{__html: `
-              <script>
+          <div className="flex w-[100vw] justify-center">
+            <div className="content">
+              <canvas
+                id="venn"
+                width="400"
+                height="400"
+                className="w-[300px] h-[300px] md:w-[450px] md:h-[450px]"
+              ></canvas>
+              {/* <script>
+              // mouse positioning not currently working in react/nextjs environment
                 document.getElementById("venn").addEventListener("mousedown", function (e) {
                   getMousePosition(document.getElementById("venn"), e)
                 });
-              </script>
-              `}} />
+              </script> */}
             </div>
           </div>
-          <div className="container">
-            <input type="checkbox" onChange={() => null} checked={true} id="headerOutput" className="css-checkbox" />
-            <label htmlFor="headerOutput" className="css-label">Output</label>
+          <div className="container w-[92vw] pl-7 md:w-[100vh]">
+            <input
+              type="checkbox"
+              onChange={() => null}
+              checked={true}
+              id="headerOutput"
+              className="css-checkbox"
+            />
+            <label htmlFor="headerOutput" className="css-label">
+              Output
+            </label>
             <div className="content css-content">
-              <div className='results' id='displaySetNotation'></div>
+              <div
+                className="results text-black bg-yellow-300 "
+                id="displaySetNotation"
+              ></div>
             </div>
           </div>
-
         </form>
       </div>
     </main>
-  )
+  );
 }
