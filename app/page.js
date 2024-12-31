a5611d85c4afe062808df700b9d932b17680c6ee
ManjeyLala <mrmanjeylala00@gmail.com>
2024-12-31 09:10:05 +0545

    FIXES: Big fixes and added instructions.

--- app/page.js
+++ app/page.js
@@ -1,113 +1,67 @@
 "use client";
 
 export default function Home() {
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main className="max-w-[1000px] w-full mx-auto p-4 h-screen items-center justify-center gap-5 hidden lg:flex flex-row">
+        <script defer type="text/javascript" src="/js/main.js"></script>
+        <script defer type="text/javascript" src="/js/mcCluskyMethod.js"></script>
+        <div id="container">
+          <form name="form1">
+            <div className="container">
+              <div className="content css-content">
+                <div className='options flex flex-row gap-2 mb-4 '>
+                  <select className=" rounded-xl p-4 bg-gray-100 border-2 border-gray-500 " id='nEvents' onChange={() => updateN() && renderVenn()}>
+                    <option id='2' value={2}>2</option>
+                    <option id='3' value={3}>3</option>
+                  </select><br />
+                  <input placeholder="Enter your formula" className=" w-full rounded-xl p-4 bg-gray-100 border-2 border-gray-500 " defaultValue={`(A'u(BnC'))'`} id="inputExpression"></input>
+                  <button type="button" className=" rounded-xl p-4 bg-blue-500 text-white " onClick={() => simplifyAndDrawExpression()} value="calculate">Calculate</button>
+                </div>
+
+              </div>
+            </div>
+            <div className='container overflow-hidden '>
+              <div className='content'>
+                <canvas className="border-0" id="venn" width="500" height="500">
+                </canvas>
+                <div dangerouslySetInnerHTML={{
+                  __html: `<script>
+                document.getElementById("venn").addEventListener("mousedown", function (e) {
+                  getMousePosition(document.getElementById("venn"), e)
+                });
+              </script>` }} />
+              </div>
+            </div>
+            <div className="container">
+              <input type="checkbox" onChange={() => null} checked={true} id="headerOutput" className="css-checkbox hidden" />
+              <label htmlFor="headerOutput" className="css-label hidden">Output</label>
+              <div className="content rounded-xl p-4 bg-gray-100 border-2 border-gray-500 mt-4 h-16 flex flex-row gap-2">
+                <p>Simplified Formula:</p>
+                <div className='results ' id='displaySetNotation'></div>
+              </div>
+            </div>
+
+          </form>
+        </div >
+        <div className=" w-full h-full flex px-8 py-12 flex-col">
+          <h1 className="text-2xl font-semibold">How to use</h1>
+          <ol className="list-decimal">
+            <li>Enter the formula using</li>
+            <li>Union: u</li>
+            <li>Intersection: n</li>
+            <li>Negation: '</li>
+            <li>Difference: -</li>
+            <li>{`For example: AnC' or (AuB)nC `}</li>
+          </ol>
+          <p className=" mt-4 ">Click on the regions to fill them and generate the formula</p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </main >
+      <main className="flex lg:hidden items-center justify-center gap-5 w-screen h-screen">
+        <h1 className="text-md font-semibold">This page is not available on mobile devices</h1>
+      </main>
+    </>
   );
 }
